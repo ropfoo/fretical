19a38ef6e3c9e985edf83c42004d6a99a4ad3680
Robert Pasdziernik <ropfoo@gmail.com>
2020-04-11 14:39:35 +0200

    initial commit

---
+++ components/Fret.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="c-fret">
+    <div v-if="checkDot()" class="c-fret__circle-container">
+      <div class="c-fret__circle-container--circle"></div>
+    </div>
+    <div v-for="n in 6" :key="n">
+      <div class="c-string">
+        <hr />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['number'],
+  methods: {
+    checkDot() {
+      if (this.number === 3 || this.number === 5 || this.number === 7) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="c-fretboard">
+      <div v-for="n in 8" :key="n">
+        <Fret :number="n" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Fret from '../components/Fret.vue';
+export default {
+  components: {
+    Fret
+  }
+};
+</script>
